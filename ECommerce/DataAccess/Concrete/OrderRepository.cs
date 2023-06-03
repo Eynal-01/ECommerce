1ab3739486a8f20f8c67b9c9eeaa9dfde4b2d56a
Baxsh_cw07 <Baxsh_cw07@STHQ012B-09>
2023-06-03 14:08:54 +0400

    Add project files.

---
+++ ECommerce/DataAccess/Concrete/OrderRepository.cs
@@ -0,0 +1,46 @@
+﻿using ECommerce.DataAccess.Abstractions;
+using ECommerce.DataAccess.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ECommerce.DataAccess.Concrete
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly EECommerceDataContext _context;
+        public OrderRepository()
+        {
+            _context = new EECommerceDataContext();
+        }
+        public void AddData(Order data)
+        {
+            _context.Orders.InsertOnSubmit(data);
+            _context.SubmitChanges();
+        }
+
+        public void DeleteData(Order data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetData(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateData(Order data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ECommerce/Domain/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.DataAccess.Abstractions;
+using ECommerce.DataAccess.Concrete;
+using ECommerce.DataAccess.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ECommerce.Domain.Services
+{
+    public class OrderService
+    {
+        private IRepository<Order> _repository;
+        public OrderService()
+        {
+            _repository = new OrderRepository();
+        }
+
+        public void AddOrder(Order order)
+        {
+            _repository.AddData(order);
+        }
+    }
+}
